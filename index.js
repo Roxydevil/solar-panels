de355ace683478e2f734f9dc74a5aea5364827c6
Roxydevil <36420732+Roxydevil@users.noreply.github.com>
2020-03-10 19:38:11 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ form.addEventListener("submit", function (e) {
 //Пример ссылки заполнения поля
 document.getElementById('primer').addEventListener("click", function () {
     document.getElementById("urlField").value =
-        "https://cors-anywhere.herokuapp.com/rozetka.com.ua/180937912/p180937912/";
+        "https://rozetka.com.ua/180937912/p180937912/";
 });
 
 //функция заполнения панели параметров
--- query_handler.php
+++ query_handler.php
@@ -1,4 +1,122 @@
 <?php
-echo "Hello world!<br>";
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+	$html = null;
+	$safeHtml = null;
+	$url = null;
+
+	$priceCount;
+
+	$pricePosition;
+	$currencyPosition;
+	$panelTypePosition;
+	$pmaxPosition;
+	$kpdPosition;
+	$imgStartPosition;
+	$imgEndPosition;
+
+	$priceWord = '"price":';
+	$currencyWord = '"priceCurrency":';
+	$panelTypeWord = 'Тип панели';
+	$pmaxWord = 'СТУ Максимальная Проектная мощность (Pmax)';
+	$kpdWord = 'КПД модуля';
+	$imgStartWord = '"image":"';
+	$imgEndWord = '","';
+
+	$price;
+	$currency;
+	$panelType;
+	$pmax;
+	$kpd;
+	$img;
+
+
+	$url = $_GET['url'];
+	$html = curl_get($url);
+		
+	//удаление тега HTML чтобы страница открывалась в браузере как текст
+	$safeHtml = strip_tags($html);
+
+	//поиск цены товара
+	$priceCount = substr_count($safeHtml, $priceWord);
+	if ($priceCount == 1) {
+		$pricePosition = strpos($safeHtml, $priceWord);
+		$price = substr($safeHtml, ($pricePosition + 9), 7); //работает только с 7 значной ценой
+	}
+	else {
+		$price = 0;
+	};
+	//поиск валюты товара
+	$currencyPosition = strpos($safeHtml, $currencyWord);
+	$currency = substr($safeHtml, ($currencyPosition + 17), 3);
+	//поиск типа панели
+	$panelTypePosition = strpos($safeHtml, $panelTypeWord);
+	$panelType = substr($safeHtml, ($panelTypePosition + 19), 22);
+	if ($panelType == 'Монокристал') {
+		$panelType = 'MonoCristal';
+	}
+	else if ($panelType == 'Поликристал') {
+		$panelType = 'PolyCristal';
+	}
+	else {
+		$panelType = 0;
+	};
+	//поиск Pmax
+	$pmaxPosition = strpos($safeHtml, $pmaxWord);
+	if ($pmaxPosition == null) {
+		$pmax = 0;
+	}
+	else {
+		$pmax = substr($safeHtml, ($pmaxPosition + 74), 3);
+	};
+	//поиск КПД
+	$kpdPosition = strpos($safeHtml, $kpdWord);
+	if ($kpdPosition == null) {
+		$kpd = 0;
+	}
+	else {
+		$kpd = substr($safeHtml, ($kpdPosition + 19), 2);
+	};
+	//Поиск адреса картинки
+	$imgStartPosition = strpos($safeHtml, $imgStartWord);
+	$imgEndPosition = strpos($safeHtml, $imgEndWord, $imgStartPosition);
+	$img = substr($safeHtml, $imgStartPosition + strlen($imgStartWord), ($imgEndPosition - $imgStartPosition) - strlen($imgStartWord));
+	
+	
+	//Добавить поиск из описания	
+	
+	
+	//формирование JSON пакета
+	$responseData = [ 
+		'Price' => $price, 
+		'Currency' => $currency,
+		'PanelType' => $panelType, 
+		'Pmax' => $pmax, 
+		'Efficiency' => $kpd, 
+		'Img' => $img
+		];
+	header('Content-Type: application/json');
+	echo json_encode($responseData);
+	
+	
+	
+			
+	//print $safeHtml;
+	//file_put_contents('1.txt', $html);
+
+	//функция curl
+	function curl_get($url, $referer = 'http://www.google.com') {
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_HEADER, 0);
+		curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:38.0) Gecko/20100101 Firefox/38.0");
+		curl_setopt($ch, CURLOPT_REFERER, $referer);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		$data = curl_exec($ch);
+		curl_close($ch);
+		return $data;
+	};
+
 
 ?>
